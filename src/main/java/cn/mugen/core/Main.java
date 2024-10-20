9681dae3e0c952bcc0be29e4571fa55ee94546e2
Mieriki <chicken1233@163.com>
2024-10-21 01:21:58 +0800

    pr测试

--- src/main/java/cn/mugen/core/Main.java
+++ src/main/java/cn/mugen/core/Main.java
@@ -2,6 +2,6 @@ package cn.mugen.core;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello Mugen!");
     }
 }
